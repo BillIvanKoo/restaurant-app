445f709c83a235b3497dede851c347482464bb9a
zanisis <adzannyaa@gmail.com>
2017-05-11 17:16:25 +0700

    send to email masih belum

--- server/helper/cronjob.js
+++ server/helper/cronjob.js
@@ -1,24 +1,59 @@
 var CronJob = require('cron').CronJob;
 require('dotenv').config({path: '../.env'})
+const User = require('../models/user');
+
 
 var kue = require('kue'),
     queue = kue.createQueue();
 
 let helper = (params)=>{
-  // console.log('helper',params.created_at);
+  console.log(params);
   let create = params.created_at
   let date = create.getDate()
-  // console.log('date', date);
   let month = create.getMonth()
-  // console.log('month', month);
   let minute = create.getMinutes() + 1
-  // console.log('minute', minute);
   let hours = create.getHours()
-  // console.log('hours', hours);
+  console.log('minute -- ',minute);
+  console.log('hour -- ', hours);
+  console.log('date -- ', date);
+  console.log('month -- ', month);
 
-  // second minute hourse date month week
-  new CronJob(`00 ${minute} ${hours} ${date} ${month} *`)
+  User.find({role : 'member'},(err, users)=>{
+    users.map((user)=>{
+      console.log('data -- ',user)
+      queue.create('email', {
+        subject : `New Promo @EATLAH`,
+        message : `Dapatkan potong 50% untuk menu baru kita : ${params.name} hanya di EATLAH, tunjukan email ini di kasir kami.  #selama masa promo`,
+        to: user.email
+      }).save((err)=>{
+        if(err) throw err
+      })
+
+      // second minute hourse date month week
+      new CronJob(`00 ${minute} ${hours} ${date} ${month} *`, function () {
+        queue.process('email', function (job, done) {
+          email(job.data, done)
+          done()
+        })
+      }, null, true, 'Asia/Jakarta')
+
+    })
+  })
+
 }
 
+function email(paramsJob, done) {
+  console.log('di email -- ',paramsJob);
+  var send = require('gmail-send')({
+    user : 'durotar.resis2@gmail.com',
+    pass : 'lbnnwbaxdraeeyiv',
+    to : paramsJob.to,
+
+    subject : paramsJob.subject,
+    text : paramsJob.message,
+  })();
+
+  // done()
+}
 
 module.exports = helper;
