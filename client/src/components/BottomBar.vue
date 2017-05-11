8c4de084a897e42bc3e81c98ee201c29186cca74
LeCodeBae <billivank_96@yahoo.co.id>
2017-05-12 00:41:22 +0700

    home, sidenav, bottombar done

---
+++ client/src/components/BottomBar.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="bb">
+    <md-dialog md-open-from="#custom" md-close-to="#custom" ref="dialog1">
+      <md-dialog-title>Sign In</md-dialog-title>
+      
+      <md-dialog-content>
+        <md-input-container>
+          <md-icon>perm_identity</md-icon>
+          <label>Username</label>
+          <md-input placeholder="Enter username"></md-input>
+        </md-input-container>
+        <md-input-container md-has-password>
+          <md-icon>lock</md-icon>
+          <label>Password</label>
+          <md-input type="password" placeholder="Enter password" ></md-input>
+        </md-input-container>
+      </md-dialog-content>
+      
+      <md-dialog-actions>
+        <md-button class="md-primary" @click.native="closeDialog('dialog1')">Cancel</md-button>
+        <md-button class="md-primary" @click.native="closeDialog('dialog1')">Submit</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+    
+    <md-dialog md-open-from="#custom" md-close-to="#custom" ref="dialog2">
+      <md-dialog-title>Sign In</md-dialog-title>
+      
+      <md-dialog-content>
+        <md-input-container>
+          <md-icon>perm_identity</md-icon>
+          <label>Username</label>
+          <md-input placeholder="Enter username"></md-input>
+        </md-input-container>
+        <md-input-container md-has-password>
+          <md-icon>lock</md-icon>
+          <label>Password</label>
+          <md-input type="password" placeholder="Enter password" ></md-input>
+        </md-input-container>
+        <md-input-container>
+          <label>Confirm Password</label>
+          <md-input type="password"></md-input>
+        </md-input-container>
+        <md-input-container>
+          <md-icon>email</md-icon>
+          <label>Email</label>
+          <md-input placeholder="Enter email" ></md-input>
+        </md-input-container>
+      </md-dialog-content>
+      
+      <md-dialog-actions>
+        <md-button class="md-primary" @click.native="closeDialog('dialog2')">Cancel</md-button>
+        <md-button class="md-primary" @click.native="closeDialog('dialog2')">Submit</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+    
+    <md-bottom-bar>
+      <md-bottom-bar-item md-icon="input" @click.native="openDialog('dialog1')">Sign In</md-bottom-bar-item>
+      <md-bottom-bar-item md-icon="account_circle" @click.native="openDialog('dialog2')">Register</md-bottom-bar-item>
+    </md-bottom-bar>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    openDialog(ref) {
+      this.$refs[ref].open();
+    },
+    closeDialog(ref) {
+      this.$refs[ref].close();
+    },
+    onOpen() {
+      console.log('Opened');
+    },
+    onClose(type) {
+      console.log('Closed', type);
+    }
+  }
+};
+</script>
+<style>
+  .bb{
+    z-index: 2;
+  }
+</style>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="home">
+    <img class="background" src="../assets/food1.png" alt="background">
+    <div class="welcome">
+      Welcome to
+    </div>
+    <div class="name">
+      ZanEAT
+    </div>
+    <div class="promo">
+      Register to get our promotions via email!!
+    </div>
+    <div class="arrow">
+      |<br>
+      |<br>
+      |<br>
+      |<br>
+      |<br>
+      |<br>
+      |<br>
+      \/<br>
+      
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+    
+  }
+</script>
+<style scoped>
+.home{
+  position: relative;
+}
+.background {
+  opacity: 0.7;
+}
+.name {
+  background-color: black;
+  position: fixed;
+  padding: 32px;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+  color: white;
+  font-size: 60px;
+  letter-spacing: 5px;
+  border-radius: 10px;
+}
+.welcome {
+  position: fixed;
+  font-size: 32px;
+  top: 40%;
+  color: black;
+  width: 100%;
+  text-align: center;
+}
+.promo{
+  position: fixed;
+  font-size: 32px;
+  top: 60%;
+  color: black;
+  width: 100%;
+  text-align: center;
+}
+.arrow{
+  position: fixed;
+  font-size: 32px;
+  top: 65%;
+  color: black;
+  left: 56%;
+}
+</style>
