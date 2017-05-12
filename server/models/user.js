9978e0f7877b4eb01108d4ebac5f6baa7e55a51c
zanisis <adzannyaa@gmail.com>
2017-05-12 10:26:34 +0700

    validate models user

--- server/models/user.js
+++ server/models/user.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema
 
 const userSchema = new Schema({
-  username: String,
-  password: String,
-  email: String,
-  role: String
+  username: {type: String, require: false},
+  password: {type: String, require: true, minlength: 5},
+  email: {type: String, require: true, match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Please fill a valid email']},
+  role: {type: String, require: false}
 })
 
 let User = mongoose.model('User', userSchema)
